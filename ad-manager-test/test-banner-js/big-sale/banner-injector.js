6037d88308f12ae432291c3784a3a11bf4b1249c
Ads10045 <132195022+git1978@users.noreply.github.com>
2026-01-04 11:49:34 +0100

    feat: automated browser language detection for test pages via data-i18n

--- ad-manager-test/test-banner-js/big-sale/banner-injector.js
+++ ad-manager-test/test-banner-js/big-sale/banner-injector.js
@@ -17,6 +17,19 @@
 
         $.getJSON(i18n.localesPath, function(translations) {
             const langData = translations[i18n.currentLang] || translations[i18n.defaultLang];
+            
+            // Étape 1.5: Traduire les éléments de la page hôte (data-i18n)
+            $('[data-i18n]').each(function() {
+                const key = $(this).data('i18n');
+                if (langData[key]) {
+                    $(this).text(langData[key]);
+                }
+            });
+
+            if (i18n.currentLang === 'ar') {
+                $('body').attr('dir', 'rtl').addClass('font-arabic');
+            }
+
             $.ajax({
                 url: `${config.apiUrl}?path=${customPath}`,
                 type: 'GET',
@@ -26,9 +39,8 @@
                         const regex = new RegExp(`\\[i18n:${key}\\]`, 'g');
                         localizedHtml = localizedHtml.replace(regex, langData[key]);
                     });
-                    localizedHtml = localizedHtml.replace('[banner_title]', langData['banner_title']);
+                    localizedHtml = localizedHtml.replace('[banner_title]', langData['banner_title'] || '');
                     $container.hide().html(localizedHtml).fadeIn(1000);
-                    if (i18n.currentLang === 'ar') $container.attr('dir', 'rtl').addClass('font-arabic');
                 }
             });
         });
